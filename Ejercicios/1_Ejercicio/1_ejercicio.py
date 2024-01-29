9f401c8ff99bc9f0ba058c204f689b9b1efe3024
EduCasanas <educasdev@gmail.com>
2024-01-29 18:02:38 -0500

    arreglo bug

--- Ejercicios/1_Ejercicio/1_ejercicio.py
+++ Ejercicios/1_Ejercicio/1_ejercicio.py
@@ -26,10 +26,12 @@ print(f"El curso actual dura {diferencia_porcentual_contra_rapido}% menos que el
 #   - el mas lento de otros cursos
 diferencia_porcentual_contra_lento = ( (tiempo_curso_maximo_horas - tiempo_curso_actual_horas) / tiempo_curso_maximo_horas ) * 100
 print("El curso actual dura {:.2f}% menos que el curso mas lento.".format(diferencia_porcentual_contra_lento))
+# El curso actual dura 78.57% menos que el curso mas lento.
 
 #   - el promedio de otros cursos
 diferencia_porcentual_contra_promedio = ( (tiempo_curso_promedio_horas - tiempo_curso_actual_horas) / tiempo_curso_promedio_horas ) * 100
 print(f"El curso actual dura {diferencia_porcentual_contra_promedio}% menos que el curso promedio.")
+# El curso actual dura 62.5% menos que el curso promedio.
 
 # B) Porcentaje de material inservible que se reduce en:
 #   - el promedio de los cursos
@@ -39,17 +41,21 @@ tiempo_curso_actual_crudo_horas = 3.5
 
 porcentaje_inservible_video_promedio = ( (tiempo_curso_promedio_crudo_horas - tiempo_curso_promedio_horas) / tiempo_curso_promedio_crudo_horas) * 100
 print(f"El porcentaje inservible que se reduce en el video promedio es: {porcentaje_inservible_video_promedio}%")
+# El porcentaje inservible que se reduce en el video promedio es: 20.0%
 
 #   - el curso actual(este curso)
 porcentaje_inservible_video_actual = ( (tiempo_curso_actual_crudo_horas - tiempo_curso_actual_horas) / tiempo_curso_actual_crudo_horas ) * 100
 print("El porcentaje inservible que se reduce en el curso actual es: {:.2f}%".format(porcentaje_inservible_video_actual))
+# El porcentaje inservible que se reduce en el curso actual es: 57.14%
 
 # C) Ver 10 horas de este curso a cuantas horas de otros cursos promedio equivale? y alreves?
 # Haciendo una regla de 3 (Ver imagen 'Ejercicio_C.1.png')
 equivalencia_horas_a_promedio = (tiempo_curso_promedio_horas * 10) / tiempo_curso_actual_horas
-print("Ver 10 horas del curso actual representan a ver {:.2f} horas de un curso promedio".format(equivalencia_horas_a_promedio)) 
+print("Ver 10 horas del curso actual representan a ver {:.2f} horas de un curso promedio.".format(equivalencia_horas_a_promedio)) 
+# Ver 10 horas del curso actual representan a ver 26.67 horas de un curso promedio.
 
 # Ahora alreves 10 horas de un curso promedio a cuanto equivale en horas al curso actual
 # Haciendo una regla de 3 (Ver imagen 'Ejercicio_C.2.png')
 equivalencia_horas_a_actual = (tiempo_curso_actual_horas * 10) / tiempo_curso_promedio_horas
-print("Ver 10 horas del curso actual representan a ver {:.2f} horas de un curso promedio".format(equivalencia_horas_a_actual)) 
+print("Ver 10 horas de un curso promedio representan a ver {:.2f} horas del curso actual.".format(equivalencia_horas_a_actual)) 
+# Ver 10 horas de un curso promedio representan a ver 3.75 horas del curso actual.
